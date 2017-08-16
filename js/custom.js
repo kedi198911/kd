ed87b4366ac49b3e8a0440a3f0de81879ef41fb0
kedi198911 <415081135@qq.com>
2017-08-16 23:53:52 +0800

    all

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+             "images/bg1.jpg",
+	 		 "images/bg2.jpg"
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
